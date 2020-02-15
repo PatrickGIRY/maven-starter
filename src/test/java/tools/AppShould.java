aeddd27152dc828c840c315fe03caaf4fb5864ce
Patrick GIRY <patrickgiry@gmail.com>
2020-02-15 22:35:30 +0100

    Add JUnit 5

---
+++ src/test/java/tools/AppShould.java
@@ -0,0 +1,14 @@
+package tools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class AppShould {
+
+    @Test
+    void should_do() {
+        assertThat(false).isFalse();
+    }
+}
